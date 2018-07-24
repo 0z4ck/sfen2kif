fa08081f0684ea35ecb7787b6d4f465e0001170e
z4ck <z4ck@m.z4ck.tk>
2018-07-24 19:08:33 +0900

    fixed promoting move's kif format

--- sftkf.py
+++ sftkf.py
@@ -51,6 +51,12 @@ for move in usimoves:
         piece = board[sfd[move[1]]][int(move[0])-1].upper()
         if move[-1]=="+":
             piece = "+"+piece
+            m = "  {} {}{}{}成({}{})   ( 0:00/00:00:00)".format(c,zenkaku[int(move[2])-1],suji[sfd[move[3]]],koma[piece],move[0],sfd[move[1]]+1)
+            moji += m + "\n"
+            board[sfd[move[3]]][int(move[2])-1] = piece
+            board[sfd[move[1]]][int(move[0])-1] = ""
+            c += 1 
+            continue
         board[sfd[move[3]]][int(move[2])-1] = piece
         board[sfd[move[1]]][int(move[0])-1] = ""
         c += 1 
