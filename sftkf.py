6ceddd73308f47f6037a7c46c84bf8ecb391b209
z4ck <z4ck@m.z4ck.tk>
2018-07-24 19:16:44 +0900

    unpromoted piece's name should be used in promoting moves

--- sftkf.py
+++ sftkf.py
@@ -48,11 +48,11 @@ for move in usimoves:
         board[sfd[move[3]]][int(move[2])-1] = piece
         m = "  {} {}{}{}打   ( 0:00/00:00:00)".format(c,zenkaku[int(move[2])-1],suji[sfd[move[3]]],koma[piece])
     else:
-        piece = board[sfd[move[1]]][int(move[0])-1].upper()
+        piece = board[sfd[move[1]]][int(move[0])-1]
         if move[-1]=="+":
-            piece = "+"+piece
-            m = "  {} {}{}{}成({}{})   ( 0:00/00:00:00)".format(c,zenkaku[int(move[2])-1],suji[sfd[move[3]]],koma[piece],move[0],sfd[move[1]]+1)
+            m = "  {} {}{}{}成({}{})   ( 0:00/00:00:00)".format(c,zenkaku[int(move[2])-1],suji[sfd[move[3]]],koma[piece.upper()],move[0],sfd[move[1]]+1)
             moji += m + "\n"
+            piece = "+"+piece
             board[sfd[move[3]]][int(move[2])-1] = piece
             board[sfd[move[1]]][int(move[0])-1] = ""
             c += 1 
@@ -60,7 +60,7 @@ for move in usimoves:
         board[sfd[move[3]]][int(move[2])-1] = piece
         board[sfd[move[1]]][int(move[0])-1] = ""
         c += 1 
-        m = "  {} {}{}{}({}{})   ( 0:00/00:00:00)".format(c,zenkaku[int(move[2])-1],suji[sfd[move[3]]],koma[piece],move[0],sfd[move[1]]+1)
+        m = "  {} {}{}{}({}{})   ( 0:00/00:00:00)".format(c,zenkaku[int(move[2])-1],suji[sfd[move[3]]],koma[piece.upper()],move[0],sfd[move[1]]+1)
     #print m
     moji += m + "\n"
 
